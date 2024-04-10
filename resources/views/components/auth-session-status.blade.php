6d37888ea3e4dc5807b795a69462d9d2256f6975
Khalil Sadik BEDAOUI <sadik.bedaoui123@gmail.com>
2024-04-10 05:38:27 +0100

    customize auth and add new guard(admin)

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,7 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600' , 'style'=> 'color: #008000']) }}>
         {{ $status }}
     </div>
 @endif
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FrontHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// FRONT DESIGN
+Route::prefix('front')->name('front.')->group(function () {
+    Route::get('/', FrontHomeController::class)->name('index');
+});
+require __DIR__ . '/auth.php';
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
